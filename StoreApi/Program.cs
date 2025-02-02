c6660fc07b43ba5d2e1c192bf39dca6d55a5b297
JeremyRodriguez <rodriguezp.jeremy@gmail.com>
2025-02-02 12:45:28 -0500

    Add Swagger configuration with JWT Bearer authentication
    Included Microsoft.OpenApi.Models namespace for OpenAPI/Swagger.
    Enhanced AddSwaggerGen to configure Swagger with "Mi API" v1.
    Integrated JWT Bearer authentication into Swagger UI.
    Set up Swagger UI to require Bearer token for API access.

--- StoreApi/Program.cs
+++ StoreApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using StoreApi.Data;
 using System.Text;
 
@@ -41,7 +42,37 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+
+        // Configurar autenticación en Swagger
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Ingrese el token en el formato: Bearer {token}"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+    });
 
 var app = builder.Build();
 
